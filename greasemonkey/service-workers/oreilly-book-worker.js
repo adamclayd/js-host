338b3fbfb2eeb63927464ef9d33132d75856b85b
Adam <adamclayd@gmail.com>
2023-08-17 17:37:54 -0500

    added functionality to oreilly book service worker

--- greasemonkey/service-workers/oreilly-book-worker.js
+++ greasemonkey/service-workers/oreilly-book-worker.js
@@ -21,6 +21,34 @@ onconnect = function (ev) {
                 downloads[msg.key] = msg.data;
             }
         }
+        else if(msg.action === 'delete-download') {
+            if(downloads[msg.key])
+                delete downloads[msg.key];
+        }
+        else if(msg.action === 'request-progress') {
+            if(downloads[msg.key]) {
+                let download = downloads[msg.key];
+                let progress = {
+                    value: Math.floor((download.pages.length / download.numPages) * 100),
+                    numPages: download.pages.length
+                }
+
+                port.postMessage({action: 'report-progress', progress: progress});
+            }
+            else {
+                port.postMessage({action: 'report-progress', progress: {}});
+            }
+        }
+        else if(msg.action === 'abort-download') {
+            let download = downloads[msg.key];
+            if(download)
+                download.aborted = true;
+        }
+        else if(msg.action === 'add-page') {
+            let download = downloads[msg.key];
+            if(download)
+                download.pages.push(msg.data);
+        }
     });
 
     port.start();
